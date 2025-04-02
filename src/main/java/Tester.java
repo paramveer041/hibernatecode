b4dbf43ff3e6996f1e4158a7bccde27d146b38cc
paramveer041 <luffynami623@gmail.com>
2025-04-02 11:26:45 +0530

    hibernate code

---
+++ src/main/java/Tester.java
@@ -0,0 +1,27 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class Tester {
+	public static void main(String[] args) {
+		
+		Configuration configuration=new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		System.out.println("21");
+		SessionFactory sessionFactory=configuration.buildSessionFactory();
+		Session session=sessionFactory.openSession();
+		Transaction transaction=session.beginTransaction();          
+		Employee employee=new Employee();
+		employee.setId(10);
+		employee.setName("hibernate");
+		session.save(employee);
+		transaction.commit();
+		session.close();
+		System.out.println("Data Inserted Successfully");
+		
+		}
+
+}
